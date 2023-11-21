661780626d2fc434a649b595e15907173dc880ae
Usama Yousaf <usamayousaf177@gmail.com>
2023-11-21 00:40:00 +0000

    22 Blade Templates

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<div>Hello I am a blade template!</div>
+
+@isset($name)
+    <div>The name is {{ $name }}</div>
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Main Page';
+    return view('index', [
+        'name'=> 'Usama'
+    ]);
 });
